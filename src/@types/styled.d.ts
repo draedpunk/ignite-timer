03934379bf2748342ef851f9e115cc9212f0f860
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-07-11 22:43:46 -0300

    att readme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,6 +4,5 @@ import { defaultTheme } from '../styles/themes/default';
 type ThemeType = typeof defaultTheme;
 
 declare module 'styled-components' {
-     
     export interface DefaultTheme extends ThemeType {}
 }
