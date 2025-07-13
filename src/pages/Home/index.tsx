ec9e7330824c2105a781e1dab21988722f95d596
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-07-13 17:23:44 -0300

    fim de formulario:3

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,29 @@
 import { Play } from "phosphor-react";
 import { Colon, CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, StartButton, TaskInput } from "./styles";
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
+import {  zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1,'Informe a tarefa'),
+    minutesAmount: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home(){
 
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    });
 
-    function handleCreateNewCycle(data){
-        console.log(data)
+    function handleCreateNewCycle(data: NewCycleFormData){
+        console.log(data);
+        reset();
     }
 
     const task = watch('task');
