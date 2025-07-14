296fb80d527503880fc94d8416680e2d23f301ff
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-07-13 21:46:14 -0300

    começando o countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { Colon, CountdownContainer, FormContainer, HomeContainer, MinutesAmountI
 import { useForm } from "react-hook-form";
 import {  zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+import { act, useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1,'Informe a tarefa'),
@@ -11,7 +12,20 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+    id: string,
+    task: string,
+    minutesAmount: number,
+    isActive: boolean
+}
+
 export function Home(){
+
+    const [cycles, setCycles] = useState<Cycle[]>([]);
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+
 
     const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
@@ -22,9 +36,33 @@ export function Home(){
     });
 
     function handleCreateNewCycle(data: NewCycleFormData){
-        console.log(data);
+        const id = String(new Date().getTime());
+
+        const newCycle: Cycle ={
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount
+        }
+        setCycles((state) => [...cycles, newCycle]);
+        setActiveCycleId(id);
+
+
         reset();
     }
+
+    const activeCycle = cycles.find(cycle => cycle.id == activeCycleId);
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+    const minutesAmount = Math.floor(currentSeconds / 60);
+    const secondsAmount = currentSeconds % 60;
+
+    // preenche uma string de tamanho especifico com um certo caracter; nesse caso, o 0 na frente para 
+    // todos os numeros menores que 10
+    const minutes = String(minutesAmount).padStart(2, '0');
+    const seconds = String(secondsAmount).padStart(2, '0');
+
 
     const task = watch('task');
     const isSubmitDisabled= !task;
@@ -63,11 +101,11 @@ export function Home(){
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Colon>:</Colon>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
                 <StartButton disabled={isSubmitDisabled} type="submit">
